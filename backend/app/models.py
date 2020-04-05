064bce25774dccac34763264374e116f5cbc82df
lukekoko <lukekoko1@gmail.com>
2020-04-05 21:31:21 +1000

    database model changes

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,9 +4,9 @@ from app.database import Base
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
-    email = Column(String(120), unique=True)
-    password = Column(String(100), unique=False)
+    name = Column(String(50), nullable=False)
+    email = Column(String(120), unique=True, nullable=False)
+    password = Column(String(100), unique=False, nullable=False)
     
     def __init__(self, name=None, email=None, password=None):
         self.name = name
