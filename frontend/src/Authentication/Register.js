c9d3b7e6f5f70dc9e31bf5c6e81455d161dff88b
hashem90 <hashem.el90@gmail.com>
2020-04-12 15:20:28 +1000

    can select user type when registering

--- frontend/src/Authentication/Register.js
+++ frontend/src/Authentication/Register.js
@@ -2,27 +2,29 @@ import React, { Component } from "react";
 import Cookies from "js-cookie";
 import { withRouter } from "react-router-dom";
 import axios from "axios";
-import { getToken } from './JwtConfig';
+import { getToken } from "./JwtConfig";
 
 class Register extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      name: '',
-      email: '',
-      password: '',
-      RegistrationSuccessful: Boolean
+      name: "",
+      email: "",
+      password: "",
+      RegistrationSuccessful: Boolean,
+      userType: "Employee",
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleRegister = this.handleRegister.bind(this);
+    this.onRadioChange = this.onRadioChange.bind(this);
   }
 
   componentDidMount() {
     const jwt = getToken();
     if (jwt) {
-      this.props.history.push('/Home');
+      this.props.history.push("/Home");
     }
   }
 
@@ -53,18 +55,32 @@ class Register extends Component {
         name: this.state.name,
         email: this.state.email,
         password: this.state.password,
-      }).then((res) => {
-        Cookies.set("auth-cookie", res.data.access_token);
-        this.props.history.push("/Home");
-      },
-      (error) => {
-        this.setState({RegistrationSuccessful:false});
-      });
+        userType: this.state.userType
+      })
+      .then(
+        (res) => {
+          Cookies.set("auth-cookie", res.data.access_token);
+          this.props.history.push("/Home");
+        },
+        (error) => {
+          this.setState({ RegistrationSuccessful: false });
+        }
+      );
   }
 
-  emptyFields(){
-      return this.state.name == '' || this.state.email == '' || this.state.password == '';
+  emptyFields() {
+    return (
+      this.state.name == "" ||
+      this.state.email == "" ||
+      this.state.password == ""
+    );
   }
+
+  onRadioChange = (e) => {
+    this.setState({
+      userType: e.target.value,
+    });
+  };
 
   render() {
     return (
@@ -100,13 +116,38 @@ class Register extends Component {
                 name="password"
               ></input>
             </div>
+            <div class="control">
+              <label class="radio">
+                <input
+                  type="radio"
+                  checked={this.state.userType === "Employee"}
+                  onChange={this.onRadioChange}
+                  value="Employee"
+                />
+                Employee
+              </label>
+              <label class="radio">
+                <input
+                  type="radio"
+                  checked={this.state.userType === "Manager"}
+                  onChange={this.onRadioChange}
+                  value="Manager"
+                />
+                Manager
+              </label>
+            </div>
           </div>
-          <button style={this.submitButton} onClick={this.handleRegister} disabled={this.emptyFields()} class="button is-primary">
+          <button
+            style={this.submitButton}
+            onClick={this.handleRegister}
+            disabled={this.emptyFields()}
+            class="button is-primary"
+          >
             Register
           </button>
-          { this.state.RegistrationSuccessful == false &&
-              <h1>Registration failed</h1>
-            }
+          {this.state.RegistrationSuccessful == false && (
+            <h1>Registration failed</h1>
+          )}
         </div>
       </div>
     );
