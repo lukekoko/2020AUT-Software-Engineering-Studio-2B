39b0459cdd85c3b15658881ce34c1af105b0d900
lukekoko <lukekoko1@gmail.com>
2020-05-07 19:58:45 +1000

    Add bash script venv check

--- build.sh
+++ build.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
+
+# frontend
 cd frontend
 npm install
 npm start & pids=$!
 cd ..
 
+# backend
 cd backend
-source venv/scripts/activate
+if [ -d "venv" ]; then
+source ./venv/scripts/activate
 python -m pip install -r requirements.txt
+else
+python -m pip install venv
+python -m venv venv
+source ./venv/scripts/activate
+python -m pip install -r requirements.txt
+fi
 python run.py & pids+=" $!"
 
 trap "kill $pids" SIGTERM SIGINT
