ed5563c2e95a2c8b5c1b03b4c8033a2e8b62fcb2
Krijan21 <krijancool.prajapati@gmail.com>
2020-05-28 12:39:57 +1000

    todolist

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -10,6 +10,8 @@ export default class Home extends Component {
     super(props);
     this.state = {
       user: {},
+      newItem:"",
+      list: [],
     };
 
     axios
@@ -21,6 +23,49 @@ export default class Home extends Component {
         Cookies.set("username", res.data['name']);
         Cookies.set("userid", res.data['id']);
       });
+
+
+  }
+
+  updateInput(key, value){
+    //update react state
+    this.setState({
+      [key]: value
+    });
+  }
+
+  addItem(){
+    //Create item with unique id
+
+    const newItem={
+      id: 1+ Math.random(),
+      value: this.state.newItem.slice()
+    };
+
+    //copy of current list of items
+    const list = [...this.state.list];
+
+    //add new item to list
+    list.push(newItem);
+
+    //update state with new list and reset newitem input
+
+    this.setState({
+      list,
+      newItem:""
+    });
+
+  }
+  deleteItem(id){
+    //copy current list of items
+    const list = [...this.state.list];
+
+    //filter out item being deleted 
+    const updatedList = list.filter(item => item.id !== id);
+
+    this.setState({list: updatedList});
+
+
   }
 
   render() {
@@ -37,6 +82,45 @@ export default class Home extends Component {
                 <p>Email: {this.state.user.email}</p>
                 <p>Name: {this.state.user.name}</p>
               </div>
+
+
+              {/* start here */}
+
+              <div className="Home">
+                <div>
+                  Add an Item..
+                  <br/>
+                  <input
+                    type="text"
+                    placeholder="Type item here..."
+                    value={this.state.newItem}
+                    onChange={e => this.updateInput("newItem",e.target.value)}
+                  />
+                  <button
+                    onClick={() => this.addItem()}
+                  >
+                    Add
+                  </button>
+                  <br/>
+                  <ul>
+                  {this.state.list.map(item => {
+                    return(
+                      <li key={item.id}>
+                        {item.value}
+                        <button
+                          onClick={() => this.deleteItem(item.id)}
+                        >
+                        x 
+                        </button>
+                      </li>
+                    )
+                  })}  
+                  </ul>
+
+                </div>
+
+              </div>
+            
             </div>
           </section>
         </div>
