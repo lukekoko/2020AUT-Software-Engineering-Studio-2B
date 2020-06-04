86cba44088d45e9e8a305058e831451948289a9d
Aviraj Attri <avi9@vt.edu>
2020-06-05 01:49:43 +1000

    added permission access for the  manager to add users to team

--- frontend/src/TeamList/Team.js
+++ frontend/src/TeamList/Team.js
@@ -10,29 +10,41 @@ class Team extends Component {
 
     this.state = {
       team: this.props.location.team,
-      users: [],
+      user: [],
+      users:[],
       employeId: [],
     };
 
-    //function to receive teams data
     axios
-      .get("/users", { headers: { Authorization: getHeaderToken() } })
+    .get("/users", { headers: { Authorization: getHeaderToken() } })
+    .then((res) => {
+      if (res.data) {
+        res.data.map((item) =>
+          this.setState({
+            users: this.state.users.concat({
+              key: item.id,
+              text: item.name,
+              value: item.id,
+            }),
+          })
+        );
+
+        console.log(res.data);
+      }
+    });
+
+  }
+  componentDidMount() {
+    axios
+      .get("/protected", { headers: { Authorization: getHeaderToken() } })
       .then((res) => {
-        if (res.data) {
-          res.data.map((item) =>
-            this.setState({
-              users: this.state.users.concat({
-                key: item.id,
-                text: item.name,
-                value: item.id,
-              }),
-            })
-          );
-          // console.log(res.data);
-        }
+        this.setState({
+          user: res.data,
+          userType: res.data["userType"],
+        });
+        console.log("NAVBAR", res.data["userType"]);
       });
   }
-
   render() {
     return (
       <div>
@@ -46,7 +58,7 @@ class Team extends Component {
               });
             }}
           >
-            Get back to list of the teams
+             ← Get back to list of the teams
           </a>
           <div>
             <section class="hero">
@@ -55,61 +67,69 @@ class Team extends Component {
                   <p>Here you can view the members of the teams</p>
                   <br />
                   <div class="ui cards">
-                    {Object.keys(this.state.team).map((key) => (
-                       this.state.employeId.push(
-                        this.state.team[key].id
-                      ),
-                      <div key={key} class="card">
-                        <div class="content">
-                          <div class="header">{this.state.team[key].name}</div>
-                          <div class="meta">
-                           
-                            Employee ID : {this.state.team[key].id}{" "}
+                    {Object.keys(this.state.team).map(
+                      (key) => (
+                        this.state.employeId.push(this.state.team[key].id),
+                        (
+                          <div key={key} class="card">
+                            <div class="content">
+                              <div class="header">
+                                {this.state.team[key].name}
+                              </div>
+                              <div class="meta">
+                                Employee ID : {this.state.team[key].id}{" "}
+                              </div>
+                              <div class="description">
+                                Sameple lora ipsum Elliot Fu pewDePie is a film-maker from New York.
+                              </div>
+                            </div>
                           </div>
-                          <div class="description">
-                            Elliot Fu is a film-maker from New York.
-                          </div>
-                        </div>
-                      </div>
-                    ))}
+                        )
+                      )
+                    )}
                   </div>
                 </div>
               </div>
             </section>
- 
-            {/* functionality to add users */}
-            <section class="hero">
-              <div class="hero-body">
-                <div>
-                  <p>Here you can add new memebers to the team</p>
-                  <br />
-                  {/* ---------------------------------------------- */}
-                  {/* list of new unassigned team memberes */}
+            {this.state.user.userType ? (
+              <div>
+                {/* functionality to add users */}
+                <section class="hero">
+                  <div class="hero-body">
+                    <div>
+                      <p>Here you can add new memebers to the team</p>
+                      <br />
+                      {/* ---------------------------------------------- */}
+                      {/* list of new unassigned team memberes */}
 
-                  {Object.keys(this.state.users).map((key) => (
-                    <div class="ui middle aligned divided list">
-                      <div class="item">
-                        <div class="right floated content">
-                          <div 
-                           onClick={() => {
-                             //send item to backend
-                          }}
-                          
-                          class="ui button">Add</div>
-                        </div>
-                        <img class="ui avatar image" src={logo} />
+                      {Object.keys(this.state.users).map((key) => (
+                        <div class="ui middle aligned divided list">
+                          <div class="item">
+                            <div class="right floated content">
+                              <div
+                                onClick={() => {
+                                  //send item to backend
+                                }}
+                                class="ui button"
+                              >
+                                Add
+                              </div>
+                            </div>
+                            <img class="ui avatar image" src={logo} />
 
-                        <div class="content">
-                          {this.state.users[key].text}
+                            <div class="content">
+                              {this.state.users[key].text}
+                            </div>
+                          </div>
                         </div>
-                      </div>
+                      ))}
+
+                      {/* -------------------------------------------------- */}
                     </div>
-                  ))}
-
-                  {/* -------------------------------------------------- */}
-                </div>
+                  </div>
+                </section>
               </div>
-            </section>
+            ): (<div></div>)}
           </div>
         </div>
       </div>
